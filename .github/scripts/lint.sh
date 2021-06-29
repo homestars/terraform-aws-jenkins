eef375ac43009989a8ba6bc21ca4e05ae772c026
Adam Martin <adam.martin@homestars.com>
2021-06-29 09:54:29 -0400

    [PF-1588] Fixing path to tflint config

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -2,4 +2,4 @@ tflint --init
 terraform fmt -recursive -write=true -list=true
 terraform init -backend=false;
 terraform validate;
-tflint -f compact -c ../.tflint.hcl;
+tflint -f compact -c .tflint.hcl;
